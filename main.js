65b19cb6b2169093214c9e30cbbc862a4c6e9d7a
AndrewChicken888 <andrewchicken888@gmail.com>
2019-03-15 22:37:08 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -3,9 +3,13 @@ function isMobileDevice() {
     return (typeof window.orientation !== "undefined") || (navigator.userAgent.indexOf('IEMobile') !== -1);
 };
 
-//Cookie tools (borrowed from w3schools)
 function setCookie(cname) {
-	alert(cname);
+	document.getElementById("newName").focus();
+	document.getElementById("newName").value = "";
+	document.getElementById("newCategory").value = "";
+	if (cname.indexOf("|") == 0 || cname.indexOf("|") == cname.length-1) {
+		document.getElementById("error").innerHTML = "Please fill in both boxes.";
+	} else {
   var d = new Date();
   d.setTime(d.getTime() + (1825*24*60*60*1000));
   var expires = "expires=" + d.toGMTString();
@@ -46,6 +50,7 @@ function setCookie(cname) {
 				nameAll.push(tempName);
 				catAll.push(false);
 				addCheckbox(tempName,nameAll.length-1);
+	}
 }
 
 function getCookie(cname) {
@@ -85,10 +90,10 @@ var nameFastFood = ["Arby's", "BoJangle's", "Chick Fil'A", "McDonald's", "Taco B
 var nameChainSitdown = ["Applebee's", "TGIFriday's", "Chipolte", "O'Charlie's", "Fazoli's", "Olive Garden", "Chili's"];
 var nameMexican = ["Taco Bell", "Taco John's", "Salsa Rita's", "Moe's"];
 var nameItalian = ["Fazoli's", "Olive Garden"];
-var nameOriental = [];
+var nameOriental = ["P.F. Chang's"];
 var nameAmerican = ["Arby's", "McDonald's", "Wendy's", "Applebee's", "TGIFriday's", "Chipolte", "O'Charlie's", "Chili's"];
 var nameChicken = ["BoJangle's", "Chick Fil'A", "Zaxby's", "KFC"];
-var nameAll = ["Arby's", "BoJangle's", "Chick Fil'A", "McDonald's", "Taco Bell", "Wendy's", "Zaxby's", "KFC", "Applebee's", "TGIFriday's", "Chipolte", "O'Charlie's", "Fazoli's", "Olive Garden", "Chili's", "Taco John's", "Salsa Rita's", "Moe's"];
+var nameAll = ["Arby's", "BoJangle's", "Chick Fil'A", "McDonald's", "Taco Bell", "Wendy's", "Zaxby's", "KFC", "Applebee's", "TGIFriday's", "Chipolte", "O'Charlie's", "Fazoli's", "Olive Garden", "Chili's", "Taco John's", "Salsa Rita's", "Moe's", "P.F. Chang's"];
 var nameCookie = [];
 
 //Arrays to store the checked/unchecked values of each restaurant in a category respective to their names in the arrays above
@@ -96,10 +101,10 @@ var catFastFood = [false, false, false, false, false, false, false, false];
 var catChainSitdown = [false, false, false, false, false, false, false];
 var catMexican = [false, false, false, false];
 var catItalian = [false, false];
-var catOriental = [];
+var catOriental = [false];
 var catAmerican = [false, false, false, false, false, false, false, false];
 var catChicken = [false, false, false, false];
-var catAll = [false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false];
+var catAll = [false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false];
 
 //Temporary array that will update categories being checked or not based on if all restaurants in that category are checked
 var tempCheck = [true];
@@ -160,12 +165,25 @@ function getCookies() {
 }
 
 //This function adds a new restaurant checkboxes
+var actualName = "";
+var pos = 0;
 function addCheckbox(name, number) {
-	document.getElementById("restaurants").innerHTML += "<input type='checkbox' id='r"+number+"' name='"+name+"' value='"+name+"'><label for='r"+number+"'> "+name+"</label><br>";
+	/*
+	for (i=0;i<name.length;i++) {
+		if (name.charAt(i)=="'") {
+			pos++;
+			actualName += "\\";
+		}
+		//actualname += name.charAt(i);
+		pos++;
+	}*/
+	document.getElementById("restaurants").innerHTML += "<input type='checkbox' id=\""+name+"\" name=\""+name+"\" value='"+name+"'><label for='r"+number+"'> "+name+"</label><br>";
 }
 
 //Runner function
 function init() {
+	document.getElementById("newName").focus();
+	
 	//Retrieve cookies
 	getCookies();
 	
@@ -181,20 +199,31 @@ function init() {
 	if (!isMobileDevice()) {
 		document.getElementById("cCont").style.float = "left";
 		document.getElementById("rCont").style.float = "left";
+		//Event listener on the input button so it can be pressed on enter:
+		document.getElementById("newName").addEventListener("keyup", function(event) {
+			if (event.keyCode === 13) {
+				setCookie(document.getElementById('newName').value + '|' + document.getElementById('newCategory').value.toLowerCase());
+			}
+		});
+		document.getElementById("newCategory").addEventListener("keyup", function(event) {
+			if (event.keyCode === 13) {
+				setCookie(document.getElementById('newName').value + '|' + document.getElementById('newCategory').value.toLowerCase());
+			}
+		});
 	}
 	
 	window.setInterval(function(){
 		//Graphic update to hide or show the categories and restaurants
 		if (cVisible && cHeight < 192) {
-			cHeight += 48;
+			cHeight += 24;
 		} else if (!cVisible && cHeight > 0) {
-			cHeight -= 48;
+			cHeight -= 24;
 		}
 		document.getElementById("categories").style.height = cHeight+"px";
 		if (rVisible && rHeight < 24*nameAll.length) {
-			rHeight += 48;
+			rHeight += 24;
 		} else if (!rVisible && rHeight > 0) {
-			rHeight -= 48;
+			rHeight -= 24;
 		}
 		document.getElementById("restaurants").style.height = rHeight+"px";		
 		//Check to make sure all checkboxes are updated		
@@ -204,35 +233,40 @@ function init() {
 		}
 		document.getElementById("c1").checked = verifyTrue(tempCheck);
 		tempCheck = [true];
+		for (i=0;i<catChainSitdown.length;i++) {
+			tempCheck.push(checkIfChecked(nameChainSitdown[i]));			
+		}
+		document.getElementById("c2").checked = verifyTrue(tempCheck);
+		tempCheck = [true];
 		for (i=0;i<catMexican.length;i++) {
 			tempCheck.push(checkIfChecked(nameMexican[i]));			
 		}
-		document.getElementById("c2").checked = verifyTrue(tempCheck);
+		document.getElementById("c3").checked = verifyTrue(tempCheck);
 		tempCheck = [true];
 		for (i=0;i<catItalian.length;i++) {
 			tempCheck.push(checkIfChecked(nameItalian[i]));			
 		}
-		document.getElementById("c3").checked = verifyTrue(tempCheck);
+		document.getElementById("c4").checked = verifyTrue(tempCheck);
 		tempCheck = [true];
 		for (i=0;i<catOriental.length;i++) {
 			tempCheck.push(checkIfChecked(nameOriental[i]));			
 		}
-		document.getElementById("c4").checked = verifyTrue(tempCheck);
+		document.getElementById("c5").checked = verifyTrue(tempCheck);
 		tempCheck = [true];
 		for (i=0;i<catAmerican.length;i++) {
 			tempCheck.push(checkIfChecked(nameAmerican[i]));			
 		}
-		document.getElementById("c5").checked = verifyTrue(tempCheck);
+		document.getElementById("c6").checked = verifyTrue(tempCheck);
 		tempCheck = [true];
 		for (i=0;i<catChicken.length;i++) {
 			tempCheck.push(checkIfChecked(nameChicken[i]));			
 		}
-		document.getElementById("c6").checked = verifyTrue(tempCheck);
+		document.getElementById("c7").checked = verifyTrue(tempCheck);
 		tempCheck = [true];
 		for (i=0;i<catAll.length;i++) {
 			tempCheck.push(checkIfChecked(nameAll[i]));			
 		}
-		document.getElementById("c7").checked = verifyTrue(tempCheck);
+		document.getElementById("c8").checked = verifyTrue(tempCheck);
 	}, 20);
 }
 
@@ -240,7 +274,6 @@ function init() {
 function verifyTrue(testArray) {
 	for (i=0;i<testArray.length;i++) {
 		if (!testArray[i]) {
-			alert("hi");
 			return false;
 		}
 	}
@@ -252,38 +285,49 @@ var choice = "null";
 var previousChoice = "null";
 function generate() {
 	var choiceList = [];
-	for (i=1;i<=15;i++) {
-		if (checkIfChecked("r"+i)) {
-			choiceList.push("r"+i);
+	for (i=0;i<nameAll.length;i++) {
+		if (checkIfChecked(nameAll[i])) {
+			choiceList.push(nameAll[i]);
 		}		
 	}
-	while (choice == previousChoice) {
+	if (choiceList.length > 1) {
+		while (choice == previousChoice) {
+			choice = document.getElementById(choiceList[Math.floor(Math.random()*choiceList.length)]).name;
+		}
+	} else {
 		choice = document.getElementById(choiceList[Math.floor(Math.random()*choiceList.length)]).name;
 	}
 	previousChoice = choice;
 	document.getElementById("answer").innerHTML = '<p style="color:#ffe500;">You should go to ' + choice + '!';
 }
 
+function hideCookies() {
+	document.getElementById("cookies").style.height = "0px";
+}
+
 function updateCategoryArray(array) {
-	//Update category array]
+	//Update category array
 	var arrLength = 0;
 	switch(array) {
 		case 1:
 			arrLength = catFastFood.length;
 			break;
 		case 2:
-			arrLength = catMexican.length;
+			arrLength = catChainSitdown.length;
 			break;
 		case 3:
-			arrLength = catItalian.length;
+			arrLength = catMexican.length;
 			break;
 		case 4:
-			arrLength = catOriental.length;
+			arrLength = catItalian.length;
 			break;
 		case 5:
-			arrLength = catAmerican.length;
+			arrLength = catOriental.length;
 			break;
 		case 6:
+			arrLength = catAmerican.length;
+			break;
+		case 7:
 			arrLength = catChicken.length;
 			break;
 		default:
@@ -296,18 +340,21 @@ function updateCategoryArray(array) {
 				document.getElementById(nameFastFood[i]).checked = checkedBool;
 				break;
 			case 2:
-				document.getElementById(nameMexican[i]).checked = checkedBool;
+				document.getElementById(nameChainSitdown[i]).checked = checkedBool;
 				break;
 			case 3:
-				document.getElementById(nameItalian[i]).checked = checkedBool;
+				document.getElementById(nameMexican[i]).checked = checkedBool;
 				break;
 			case 4:
-				document.getElementById(nameOriental[i]).checked = checkedBool;
+				document.getElementById(nameItalian[i]).checked = checkedBool;
 				break;
 			case 5:
-				document.getElementById(nameAmerican[i]).checked = checkedBool;
+				document.getElementById(nameOriental[i]).checked = checkedBool;
 				break;
 			case 6:
+				document.getElementById(nameAmerican[i]).checked = checkedBool;
+				break;
+			case 7:
 				document.getElementById(nameChicken[i]).checked = checkedBool;
 				break;
 			default:
